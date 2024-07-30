e25e58447aa35e0ddc37c3b066db26f1419ac8db
nicejs-is-cool <nicejsalt@gmail.com>
2024-07-30 19:40:21 -0300

    libipc: add waiting req

--- libipc/ipc.lua
+++ libipc/ipc.lua
@@ -1,6 +1,7 @@
 local event = require("event")
 local t = {
-	_registered = {}
+	_registered = {},
+	_waiting_reg = {}
 }
 function t:create(name)
 	self._registered[name] = {
@@ -9,8 +10,17 @@ function t:create(name)
 	return {
 		name = name,
 		_getq = function(_self) return self._registered[_self.name] end,
+		_gett = function() return self end
 		register = function(self, cb)
 			self:_getq().cb = cb
+			local a = self._gett()._waiting_req
+			if a[self.name] ~= nil then
+				for k, v in ipairs(a[self.name])
+				do
+					v()
+				end
+				a[self.name] = nil
+			end
 		end,
 		unregister = function(self) self:_getq().cb = nil end,
 		destroy = function(_self)
@@ -20,7 +30,7 @@ function t:create(name)
 end
 
 function t:exists(name)
-	if self[name] ~= nil then return true end
+	if self._registered[name] ~= nil then return true end
 	return false
 end
 
@@ -37,6 +47,11 @@ function t:get(name)
 			return true, self:call(...)
 		end,
 	}
+end
+
+function t:wait_create(name, cb)
+	if self._waiting_req[name] ~= nil then self._waiting_req[name] = {} end
+	table.insert(self._waiting_req[name], cb)
 end
 
 return t
